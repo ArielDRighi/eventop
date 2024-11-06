951f95063af31eed990d14fe4342edcf13c694be
Ariel Righi <arieldavidrighi@gmail.com>
2024-11-06 09:29:40 -0300

    SCRUM-7 JWT Integration
    Se creo la logica para proteger rutas

--- back/eventop/src/auth/auth.controller.ts
+++ back/eventop/src/auth/auth.controller.ts
@@ -1,9 +1,19 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UseGuards,
+  Request,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignInAuthDto } from './dto/signIn.dto';
 import { CreateUserDto } from './dto/createUser.dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
+//@UseGuards(AuthGuard('jwt'))
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
@@ -18,5 +28,11 @@ export class AuthController {
   @HttpCode(HttpStatus.CREATED)
   signUp(@Body() user: CreateUserDto) {
     return this.authService.signUp(user);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Post('profile')
+  getProfile(@Request() req) {
+    return req.user;
   }
 }
--- back/eventop/src/users/users.controller.ts
+++ back/eventop/src/users/users.controller.ts
@@ -1,9 +1,27 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  UseGuards,
+  Request,
+} from '@nestjs/common';
 import { UserService } from './users.service';
+import { AuthGuard } from '@nestjs/passport';
+import { CreateUserDto } from 'src/auth/dto/createUser.dto';
 
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  // Rutas
+  @UseGuards(AuthGuard('jwt'))
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
+  }
+
+  @Post('create')
+  async createUser(@Body() createUserDto: CreateUserDto) {
+    return this.userService.createUser(createUserDto);
+  }
 }
