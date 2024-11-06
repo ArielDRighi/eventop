992f1f5990b1ed6b69e57c2a93ae8b6c9f56d97e
federico vanni <158511163+federicovanni@users.noreply.github.com>
2024-11-06 10:03:17 -0300

    configurar entorno de docker
    se configuro el entorno de docker, se trabajo con docker compose creando los contenedores pgadmin, postgresdb y eventop permitiendo el levantamiento conjunto

--- back/eventop/src/config/typeorm.ts
+++ back/eventop/src/config/typeorm.ts
@@ -6,7 +6,7 @@ dotenvConfig({ path: '.env' });
 const config = {
   type: 'postgres',
   host: `${process.env.DB_HOST}`,
-  port: `${process.env.DB_PORT}`,
+  port: parseInt(process.env.DB_PORT, 10) || 5432,
   username: `${process.env.DB_USERNAME}`,
   password: `${process.env.DB_PASSWORD}`,
   database: `${process.env.DB_NAME}`,
