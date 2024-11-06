5584a5d763f94d65a9c03b4a0383437e4dcc2e9e
federico vanni <158511163+federicovanni@users.noreply.github.com>
2024-11-05 22:20:18 -0300

    DOCKERFILE
    configure el dockerfile

--- back/eventop/src/app.module.ts
+++ back/eventop/src/app.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import typeOrm from './config/typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+    load: [typeOrm],
+  }),
+  TypeOrmModule.forRootAsync({
+    inject: [ConfigService],
+    useFactory: (config: ConfigService) => config.get('typeOrm'),
+  }),],
   controllers: [],
   providers: [],
 })
